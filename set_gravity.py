3bd30da360519850a068f5d79b992e811c828e63
Christopher Jubb <cjubb@makielab.com>
2015-12-05 17:50:49 +0000

    Swap stuff around for left flipper.

---
+++ set_gravity.py
@@ -0,0 +1,14 @@
+import bge
+import mathutils
+import math
+
+table_angle = 10
+
+eul = mathutils.Euler((math.radians(table_angle), 0.0, 0.0), 'XYZ')
+vec = mathutils.Vector((0.0, -22.0, 0.0))
+vec.rotate(eul)
+
+print("Table angle = %s" % table_angle)
+print("Gravity = %s" % vec)
+
+bge.constraints.setGravity(vec.x, vec.z, vec.y)
