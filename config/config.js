b28e2f6bbc2eefcae3f88da1ec02c9d60eb1814b
Christopher Jubb <cjubb@makielab.com>
2015-12-07 09:40:57 +0000

    Loading will take time - have a callback system to populate after loading.

--- config/config.js
+++ config/config.js
@@ -1,21 +1,36 @@
 var Config = Backbone.Model.extend({
 
+	configJson: undefined,
+	settingsJson: undefined,
+	readyCallbacks: [],
+
 	load: function(configFilename, settingsFilename) {
 		console.log("Starting to load - " + settingsFilename);
 
 		var self = this;
+		self.settingsJson = undefined;
+		self.configJson = undefined;
 		$.getJSON(configFilename, function(configJson) {
 			$.getJSON(settingsFilename, function(settingsJson) {
 				self.processConfigAndSettings(configJson, settingsJson);
 			});
 		});
+
+		return this;
 	},
 
 	processConfigAndSettings: function(configJson, settingsJson) {
+		console.log("Completed loading");
 		this.configJson = configJson;
 		this.settingsJson = settingsJson;
 		// todo - validate against schema.
 		// todo - validate settings against the config
+
+		var self = this;
+		_.each(this.readyCallbacks, function(callback) {
+			callback.call(this, self.configJson, self.settingsJson);
+		});
+		this.readyCallbacks = [];
 	},
 
 	get: function(id) {
@@ -28,6 +43,15 @@ var Config = Backbone.Model.extend({
 
 	getConfig: function() {
 		return this.configJson;
+	},
+
+	ready: function(callback) {
+		if(this.configJson && this.settingsJson) {
+			callback(this.configJson, this.settingsJson);
+		}
+		else {
+			this.readyCallbacks.push(callback);
+		}
 	}
 });
 
@@ -35,13 +59,16 @@ var ConfigView = Backbone.View.extend({
 	render: function() {
 		this.$el.empty();
 
-		var output = "<form>";
-		_.each(this.model.getConfig(), function(entry) {
-			output += "<input id='" + entry.name + "'>" + entry.minimum + "</input>";
-		});
-		output += "</form>";
+		var self = this;
+		this.model.ready(function(config, settings) {
+			var output = "<form>";
+			_.each(config, function(entry) {
+				output += "<input id='" + entry.name + "' value='" + entry.minimum + "'></input><br>";
+			});
+			output += "</form>";
 
-		this.$el.html(output);
+			self.$el.html(output);
+		});
 
 		return this;
 	}
