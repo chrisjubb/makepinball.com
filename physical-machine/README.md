0422192c91292c56ada3f468daa51f1d665c113f
Christopher Jubb <cjubb@makielab.com>
2016-01-18 17:43:42 +0000

    Start of the physical machine interface - use Johny Five and node.js. Just blinks pin 13 at the moment.

---
+++ physical-machine/README.md
@@ -0,0 +1,13 @@
+Code to run the actual machine using node.js and Johny Five.
+
+Setup:
+```
+npm install
+```
+
+Connect an Arduino Uno and upload the FirmataStandard firmware from the Arduino IDE examples menu.
+
+Run:
+```
+node .
+```
---
+++ physical-machine/index.js
@@ -0,0 +1,11 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+// The board's pins will not be accessible until
+// the board has reported that it is ready
+board.on("ready", function() {
+	console.log("Ready!");
+
+	var led = new five.Led(13);
+	led.blink(500);
+});
